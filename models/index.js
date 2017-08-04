a4b8befbf93a24f343ea391276975261bf93bfb8
Steve Aksamit <steveaksamit@gmail.com>
2017-08-04 17:02:56 -0500

    Second commit

---
+++ models/index.js
@@ -0,0 +1,42 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+
+const Sequelize = require("sequelize");
+
+const db = new Sequelize("postgres://localhost:5432/twitter");
+
+const Page = db.define(
+  "page",
+  {
+    title: {
+      type: Sequelize.STRING
+    },
+    urlTitle: {
+      type: Sequelize.STRING
+    },
+    content: {
+      type: Sequelize.TEXT
+
+    }
+  }
+);
+
+
+const User = db.define(
+  "user",
+  {
+    name: {
+      type: Sequelize.STRING
+    },
+
+    email: {
+      type: Sequelize.STRING
+    }
+  }
+);
+
+module.exports = {
+  Page: Page,
+  User: User
+};
