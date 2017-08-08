e17c7d4b32eb270f087e231fa4b6d0ae4efd0231
Steve Aksamit <steveaksamit@gmail.com>
2017-08-07 19:50:30 -0500

    final commit with completed excercises

--- app.js
+++ app.js
@@ -1,48 +1,36 @@
-const express = require('express');
-const router = express.Router();
-const nunjucks = require('nunjucks');
-const app = express();
-const bodyParser = require('body-parser');
-const routes = require('./routes');
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var path = require('path');
 
-const models = require('./models');
+module.exports = app;
 
-// ... other stuff
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+var env = nunjucks.configure('views', { noCache: true });
+require('./filters')(env);
 
-models.User.sync({})
-.then(function () {
-    return models.Page.sync({});
-})
-.then(function () {
-    // make sure to replace the name below with your express app
-    app.listen(3000, function () {
-        console.log('listening at http://localhost:3000/');
-    });
-})
-.catch(console.error);
+//plug-in that basically tells nunjucks it’s OK to render the html in a string
+//there’s a built-in nunjucks filter that indicates this as well, but this is another option, giving you a tag so you can indicate a bunch of stuff that’s safe to render
+var AutoEscapeExtension = require("nunjucks-autoescape")(nunjucks);
+env.addExtension('AutoEscapeExtension', new AutoEscapeExtension(env));
 
-
- app.engine('html', nunjucks.render);
- app.set('view engine', 'html');
-
-
-//body parsing
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev')); //logging middleware
+app.use(express.static(path.join(__dirname, './public'))); //serving up static files (e.g. css files)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/wiki', require('./routes/wiki'));
+app.use('/users', require('./routes/users'));
 
-app.use(express.static('public'));
-
-app.use('/', routes);
-
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/', function (req, res) {
+   res.render('index');
 });
 
-// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
-// instance, which we'll want to use to add Markdown support later.
-var env = nunjucks.configure('views', {noCache: true});
-// have res.render work with html files
-app.set('view engine', 'html');
-// when res.render works with html files, have it use nunjucks to do so
-app.engine('html', nunjucks.render);
+//error handling middleware - MUST have all 4 parameters
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal Error");
+});
--- models/index.js
+++ models/index.js
@@ -1,62 +1,105 @@
 var Sequelize = require('sequelize');
+var marked = require('marked');
+
 var db = new Sequelize('postgres://localhost:5432/wikistack', {
-logging: false});
+    logging: false
+});
 
-
-const Page = db.define(
-  "page",
-  {
+var Page = db.define('page', {
     title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        max: 60,
-        notEmpty: true
-      }
+        type: Sequelize.STRING,
+        allowNull: false
     },
     urlTitle: {
-      type: Sequelize.STRING,
-      get() {
-        const route = this.getDataValue('urlTitle')
-        return '/wiki/' + route
-      }
+        type: Sequelize.STRING,
+        allowNull: false,
+        //since we are searching, editing, deleting by urlTitle, these need to be unique
+        unique: true
     },
     content: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-      validate:
-      {max: 200}
+        type: Sequelize.TEXT,
+        allowNull: false
     },
     status: {
-      type: Sequelize.ENUM('open', 'closed')
-    }
-  }
-);
-
-const User = db.define(
-  "user",
-  {
-    name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        max: 50
-      }
+        type: Sequelize.ENUM('open', 'closed')
     },
+    tags: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        defaultValue: [],
+        set: function (tags) {
 
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      }
+            tags = tags || [];
+
+            if (typeof tags === 'string') {
+                tags = tags.split(',').map(function (str) {
+                    return str.trim();
+                });
+            }
+
+            this.setDataValue('tags', tags);
+
+        }
     }
-  }
-);
+}, {
+    getterMethods: {
+        route: function () {
+            return '/wiki/' + this.urlTitle;
+        },
+        renderedContent: function () {
+            return marked(this.content);
+        }
+    },
+});
 
+Page.findByTag = function (tag) {
+    return this.findAll({
+        where: {
+            tags: {
+                $contains: [tag]
+            }
+        }
+    });
+}
+
+Page.prototype.findSimilar = function () {
+    return Page.findAll({
+        where: {
+            id: {
+                $ne: this.id
+            },
+            tags: {
+                $overlap: this.tags
+            }
+        }
+    });
+}
+
+Page.hook('beforeValidate', function (page) {
+    if (page.title) {
+        page.urlTitle = page.title.replace(/\s/g, '_').replace(/\W/g, '');
+    } else {
+        page.urlTitle = Math.random().toString(36).substring(2, 7);
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        isEmail: true,
+        allowNull: false
+    }
+});
+
+//This adds methods to 'Page', such as '.setAuthor'. It also creates a foreign key attribute on the Page table pointing ot the User table
+Page.belongsTo(User, {
+    as: 'author'
+});
 
 module.exports = {
-  db: db,
-  Page: Page,
-  User: User
+    Page: Page,
+    User: User
 };
